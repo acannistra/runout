1124454fcbcd0e6b249af60061cbbfb7a3a5f7d2
acannistra <acannistra@users.noreply.github.com>
2023-01-25 18:05:08 +0000

    deploy: 5859ac88744cde51349c9d47c5b567904f4c602a

--- _next/static/71TtXJ-0jRzimv30fe_LU/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/dd81a582-cacff80d9cde7776.js","static/css/8109db6c93368524.css","static/chunks/pages/index-df089cf90b665ef4.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/Mpup-gHak5h5wh7GwxN4h/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/dd81a582-cacff80d9cde7776.js","static/css/8109db6c93368524.css","static/chunks/pages/index-99ba05dfb4db64fd.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
