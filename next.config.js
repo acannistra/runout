3451e55ebfa3c9ddd40347bb2dca6a10ca807b6c
Tony Cannistra <tony.cannistra@gmail.com>
2023-01-25 10:41:28 -0700

    add /runout asset prefix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
+const { default: next } = require('next');
+const isProd = process.env.NODE_ENV === 'production'
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true
-  }
+  },
+  assetPrefix: isProd ? '/runout' : undefined,
 }
+
 
 module.exports = nextConfig
