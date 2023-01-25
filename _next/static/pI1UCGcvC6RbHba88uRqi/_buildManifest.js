8a04a7182dac343a22e5dfc088b829d5d0850b2f
acannistra <acannistra@users.noreply.github.com>
2023-01-25 17:36:56 +0000

    deploy: b0438b1b83055c8e84e865496e66cbff53bcc935

---
+++ _next/static/pI1UCGcvC6RbHba88uRqi/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/dd81a582-cacff80d9cde7776.js","static/css/8109db6c93368524.css","static/chunks/pages/index-525889bb02f57e25.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
